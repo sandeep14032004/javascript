722fa0233c5d8da6ef6e69f950bfdd01d7d80580
sandeep14032004 <sandeeppati69@gmail.com>
2024-07-11 14:22:08 +0000

    Add 02_datatypes.js file with basic data type examples

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+// alert(3 + 3)
+console.log("good evening");
+// number = 2,3,4
+// BigInt
+// String
+// Boolean =  true or false
+// null this is a stand alone value
+// undefined = 
+// symbol = unique
+
+// object = 
+console.log(typeof("sandeep"));
+console.log(typeof null);
+console.log(typeof undefined);
