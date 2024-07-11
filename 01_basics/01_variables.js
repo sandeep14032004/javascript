d1a0ff81778761759c92459d5e98809268c6753f
sandeep14032004 <sandeeppati69@gmail.com>
2024-07-11 06:04:34 +0000

    Update files

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,5 @@
 const accountId = 144553;
 let accountEmail = "sandeep@gmail.com";
 var password = "12345";
-
 console.log(accountEmail)
-console.table(accountId,accountEmail,password)
+console.log([accountId,accountEmail,password])
