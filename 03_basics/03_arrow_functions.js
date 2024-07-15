8b4af69ef0a7892645a8c7ccb3628f6c106f41d2
sandeep pati <sandeeppati69@gmail.com>
2024-07-15 10:16:33 +0000

    arrow functions

---
+++ 03_basics/03_arrow_functions.js
@@ -0,0 +1,23 @@
+const user = {
+    username : "sandeep",
+    price : 990,
+    welcomemessage : function(){
+        console.log(`${this.username} , welcome to website`);
+    }
+}
+// user.welcomemessage()
+// user.username = "virat"
+// user.welcomemessage()
+
+// const chai = () =>{
+//     console.log(this);
+// }
+// chai()
+
+// const sum = (num1 , num2) => {
+//     return num1 + num2
+// }
+// console.log(sum(1,2));
+
+// const sum = (n1,n2) => n1+n2
+// console.log(sum(3,4));
