a5e122f417da97669c3d0e20ee992fdaba4b0d2d
sandeep pati <sandeeppati69@gmail.com>
2024-07-15 17:25:48 +0000

    filter

--- 05_iterators/four.js
+++ 05_iterators/four.js
@@ -69,5 +69,5 @@ const books = [
 // console.log(userbook);
 
 const nums = [1,2,3,4,5,6,7,8,9,10]
-let double =  nums.map((num)=>num*2)
-console.log(double);
+// let double =  nums.map((num)=>num*2)
+// console.log(double);
