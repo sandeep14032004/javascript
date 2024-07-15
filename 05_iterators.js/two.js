7f844f9ebcce6ab382b0e38e600a8982bb22cb9d
sandeep pati <sandeeppati69@gmail.com>
2024-07-15 14:39:17 +0000

    while and do while loop

---
+++ 05_iterators.js/two.js
@@ -0,0 +1,19 @@
+// let i = 0
+// while (i <= 6) {
+//     console.log(i);
+//     i++;
+    
+// }
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let arr1 = 0
+// while (arr1 <= arr.length) {
+//     console.log(arr1);
+//     arr1++;
+// }
+
+// let score = 1
+// do{
+//     console.log(`score is ${score}`);
+//     score++;
+// }
+// while(score<11)
