707ccba8805d7acf5869583183c9cb827919b12c
sandeep pati <sandeeppati69@gmail.com>
2024-07-16 03:51:16 +0000

    filter reduce

--- 05_iterators/four.js
+++ 05_iterators/four.js
@@ -68,6 +68,44 @@ const books = [
 // const userbook = books.filter((bk) => bk.genre ==="Fantasy" && bk.edition ==="First Edition" )
 // console.log(userbook);
 
-const nums = [1,2,3,4,5,6,7,8,9,10]
-// let double =  nums.map((num)=>num*2)
-// console.log(double);
+// const nums = [1,2,3,4,5,6,7,8,9,10]
+// // let double =  nums.map((num)=>num*2)
+// // console.log(double);
+
+
+// let doubleAddOne = nums.map((num)=>num*2).map((num)=>{return num+1}).filter((num)=>num>10)
+// // console.log(doubleAddOne);
+
+// const nums1 = [1,2,3]
+// mytotal = nums.reduce((acc,currval)=>{
+//     return  acc+currval
+// },0)
+// console.log(mytotal);
+
+
+const menu = [
+    {
+        course: "Appetizer",
+        price: 2999
+    },
+    {
+        course: "Main Course",
+        price: 3999
+    },
+    {
+        course: "Dessert",
+        price: 1999
+    },
+    {
+        course: "Beverage",
+        price: 1499
+    },
+    {
+        course: "Salad",
+        price: 2499
+    }
+];
+
+
+totalPrice = menu.reduce((acc,p)=>acc+p.price,0)
+console.log(totalPrice);
